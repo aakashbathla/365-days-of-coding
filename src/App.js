b034ff1c6b1311ae99decd8a9c800f1318ae6a0e
aakbathl <aakash.bathla@sapient.com>
2019-11-03 12:50:35 +0530

    udemy hooks course | how useState Workds

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import React,{useState,useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  const [count,setCount] = useState(0);
-  useEffect(()=>{
-    document.title=`You Click ${count} times`;
-  })
-  return (
-    <div className="App">
-      <p>
-        You Clicked {count} times
-      </p>
-      <button onClick={()=>setCount(count+1)}>
-        Click Me!
-      </button>
-    </div>
-  );
-}
+import Ingredients from './components/Ingredients/Ingredients';
+
+const App = props => {
+  return <Ingredients />;
+};
 
 export default App;
-
-//It’s hard to reuse stateful logic between components
-//Complex components become hard to understand
-//Classes confuse both people and machines
-
