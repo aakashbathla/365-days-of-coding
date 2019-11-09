899fe3cde0b724240833759ad3cb7db2e682bf68
aakbathl <aakash.bathla@sapient.com>
2019-11-09 20:23:32 +0530

    custom hooks in advance

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,17 +1,17 @@
-import React, {useState} from 'react';
-import LoadingIndicator from '../UI/LoadingIndicator';
+import React, { useState } from 'react';
+
 import Card from '../UI/Card';
+import LoadingIndicator from '../UI/LoadingIndicator';
 import './IngredientForm.css';
- 
+
 const IngredientForm = React.memo(props => {
-  const [inputState, setInputState] = useState({title:'',amount:''});
-  const [enteredTitle,setEnteredTitle] = useState('');
-  const [enteredAmount,setEnteredAmount] = useState('');
-  console.log('RENDERING INGREDIENT FORM')
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  console.log('RENDERING INGREDIENT FORM');
 
   const submitHandler = event => {
     event.preventDefault();
-    props.onAddIngredient({title:enteredTitle,amount:enteredAmount})
+    props.onAddIngredient({ title: enteredTitle, amount: enteredAmount });
   };
 
   return (
@@ -20,15 +20,25 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={enteredTitle} onChange = {event => {setEnteredTitle(event.target.value)}}
-              />
+            <input
+              type="text"
+              id="title"
+              value={enteredTitle}
+              onChange={event => {
+                setEnteredTitle(event.target.value);
+              }}
+            />
           </div>
-          <div  className="form-control">
+          <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={enteredAmount} onChange={
-              event=>{
-                setEnteredAmount(event.target.value)
-                }}/>
+            <input
+              type="number"
+              id="amount"
+              value={enteredAmount}
+              onChange={event => {
+                setEnteredAmount(event.target.value);
+              }}
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
@@ -41,7 +51,3 @@ const IngredientForm = React.memo(props => {
 });
 
 export default IngredientForm;
-
-//You must use hooks inside functional component or custom hooks
-//always use hooks in the root level of the component
-
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,111 +1,98 @@
-import React,{useReducer, useEffect, useCallback} from 'react';
+import React, { useReducer, useEffect, useCallback, useMemo } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
-import Search from './Search';
 import ErrorModal from '../UI/ErrorModal';
+import Search from './Search';
 import useHttp from '../../hooks/http';
 
 const ingredientReducer = (currentIngredients, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case 'SET':
       return action.ingredients;
     case 'ADD':
-      return [...currentIngredients, action.ingredient]
+      return [...currentIngredients, action.ingredient];
     case 'DELETE':
-      return currentIngredients.filter(ing=>ing.id!==action.id);
+      return currentIngredients.filter(ing => ing.id !== action.id);
     default:
-      throw new Error('Should not get there');
+      throw new Error('Should not get there!');
   }
-}
-
-
+};
 
 const Ingredients = () => {
   const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
-  const {isLoading, error, data, sendRequest} = useHttp();
-  // const [userIngredients,setUserIngredients] = useState([]);
-  // const [isLoading, setIsloading] = useState(false);
-  // const [error, setError] = useState();
-  // useEffect(()=>{
-  //   fetch('https://react-hooks-update-daf1c.firebaseio.com/ingredients.json',{
-      
-  //   }).then(response => {
-  //     return response.json();
-  //   }).then(responseData => {
-  //     const loadedIngredients = [];
-  //     for(const key in responseData){
-  //       loadedIngredients.push({
-  //         id: key,
-  //         title: responseData[key].title,
-  //         amount: responseData[key].amount
-  //       })
-  //     }
-  //     setUserIngredients(loadedIngredients);
-  //   })
-  // },[]);
+  const {
+    isLoading,
+    error,
+    data,
+    sendRequest,
+    reqExtra,
+    reqIdentifer,
+    clear
+  } = useHttp();
 
-  useEffect(()=>{
-    console.log('Rendering Ingredients',userIngredients)
-  },[userIngredients])
-  const addIngredientHandler = useCallback(ingredient => {
-      // setIsloading(true);
-      // dispatchHttp({type: 'SEND'}); 
-      // fetch('https://react-hooks-update-daf1c.firebaseio.com/ingredients.json',{
-      //   method:'POST',
-      //   body: JSON.stringify(ingredient),
-      //   headers: {
-      //     'Content-Type':'application/json'
-      //   }
-      // }).then(response=>{
-      //   // setIsloading(false);
-      //   dispatchHttp({type: 'RESPONSE'}); 
-      //   return response.json()
-      // }).then(responseData => {
-      //   dispatch({type: 'ADD', ingredient: {id: responseData.name, ...ingredient}})
-      //   // setUserIngredients((prevIngredients)=>
-      //   //   [...prevIngredients,
-      //   //     {
-      //   //       id: responseData.name,
-      //   //       ...ingredient
-      //   //     }
-      //   //   ]
-      //   // )
-      // })
-  },[]);
+  useEffect(() => {
+    if (!isLoading && !error && reqIdentifer === 'REMOVE_INGREDIENT') {
+      dispatch({ type: 'DELETE', id: reqExtra });
+    } else if (!isLoading && !error && reqIdentifer === 'ADD_INGREDIENT') {
+      dispatch({
+        type: 'ADD',
+        ingredient: { id: data.name, ...reqExtra }
+      });
+    }
+  }, [data, reqExtra, reqIdentifer, isLoading, error]);
+
   const filteredIngredientsHandler = useCallback(filteredIngredients => {
-      // setUserIngredients(filteredIngredients);
-      dispatch({type: 'SET', ingredients: filteredIngredients})
-  },[]);
-  const removeIngredientHandler = useCallback(ingredientId => {
-    sendRequest(`https://react-hooks-update-daf1c.firebaseio.com/ingredients/${ingredientId}.json`, 'DELETE')
-    // setIsloading(true);
-    // dispatchHttp({type: 'SEND'}); 
-  },[sendRequest]);
-  const clearError = useCallback(() => {
-    // setError(null);
-    // setIsloading(false);
-    // dispatchHttp({
-    //   type: 'CLEAR'
-    // })
-  },[]);
+    dispatch({ type: 'SET', ingredients: filteredIngredients });
+  }, []);
+
+  const addIngredientHandler = useCallback(ingredient => {
+    sendRequest(
+      'https://react-hooks-update-daf1c.firebaseio.com/ingredients.json',
+      'POST',
+      JSON.stringify(ingredient),
+      ingredient,
+      'ADD_INGREDIENT'
+    );
+  }, [sendRequest]);
+
+  const removeIngredientHandler = useCallback(
+    ingredientId => {
+      sendRequest(
+        `https://react-hooks-update-daf1c.firebaseio.com/ingredients/${ingredientId}.json`,
+        'DELETE',
+        null,
+        ingredientId,
+        'REMOVE_INGREDIENT'
+      );
+    },
+    [sendRequest]
+  );
+
+  const ingredientList = useMemo(() => {
+    return (
+      <IngredientList
+        ingredients={userIngredients}
+        onRemoveItem={removeIngredientHandler}
+      />
+    );
+  }, [userIngredients, removeIngredientHandler]);
+
   return (
     <div className="App">
-      {error && <ErrorModal onClose={clearError}>
-        {error}
-      </ErrorModal>}
-      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading}/>
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
+
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
 
       <section>
-        <Search onLoadIngredients={filteredIngredientsHandler}/>
-        <IngredientList ingredients = {userIngredients} onRemoveItem={removeIngredientHandler}/>
+        <Search onLoadIngredients={filteredIngredientsHandler} />
+        {ingredientList}
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
-
-//useEffect method is similar to componentDidUpdate
-//but when we pass empty array to useEffect Method it behaves 
-//as componentDidMount
